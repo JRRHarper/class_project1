d58a13fa5835095e8844975d91c2fbd4f028f593
claire <user@Users-MacBook-Pro.local>
2016-02-24 16:38:19 +0000

    store

--- store.rb
+++ store.rb
@@ -1,9 +1,10 @@
 class Store
 
-attr_reader :pets, :till
+attr_reader :pets, :till, :customers
 
 
-  def initialize(pets, till)
+  def initialize(pets, till, customers)
+    @customers = customers
     @pets = pets
     @till = 0
   end
@@ -17,6 +18,30 @@ attr_reader :pets, :till
     return @till
   end
 
+  def add_new_customer(customer)
+    @customers.push(customer)
+  end
+
+  def updated_till_value(pet)
+    @till += pet.price
+  end
+
+  def sell_pet(pet)
+    @pets.delete(pet)
+    return @pets
+  end
+
+  def updated_bag(customer, pet)
+    customer.carry_case.push(pet)
+    return customer.carry_case
+ end
+
+  def updated_wallet(customer, pet)
+    customer.customer_wallet -= pet.price
+    return customer.customer_wallet
+  end
+
+ 
 
 
 end
