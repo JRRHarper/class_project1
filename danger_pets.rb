2f138cf091a4720c27f335ce0dba21f6247244d3
claire <user@Users-MacBook-Pro.local>
2016-02-24 11:33:45 +0000

    pets with species, price and danger level

--- danger_pets.rb
+++ danger_pets.rb
@@ -12,6 +12,11 @@ attr_reader :name, :species, :price, :danger_level
 
   end
 
+# def danger_test
+#   for pet in @pets
+#     for key, value in pet
+#       return key if value > 
 
+# end
 
 end
--- danger_pets_spec.rb
+++ danger_pets_spec.rb
@@ -6,11 +6,30 @@ require_relative './danger_pets'
 class TestPets< Minitest::Test
 
   def setup
-    @pet= DangerPets.new("Borris", "tiger", 100, 8)
+    @pet1= DangerPets.new("Borris", "tiger", 100, 8)
+    @pet2= DangerPets.new("Mulder", "polar bear", 200, 9)
+    @pets=[@pet1, @pet2]
   end
 
   def test_tiger_has_a_name
-    assert_equal("Borris", @pet.name)
+    assert_equal("Borris", @pet1.name)
   end
+
+def test_if_we_have_species
+  assert_equal("tiger", @pet1.species)
+end
+
+def test_price_tiger
+  assert_equal(100, @pet1.price)
+end
+
+def test_tiger_danger_level
+  assert_equal(8, @pet1.danger_level)
+end
+
+# def test_polar_bear_v_tiger
+# assert_equal(@pet2, @pets.danger_test)
+# end
+
 
 end
